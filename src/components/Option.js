ea3ba39afc4dc792710dafa1df0b33e5f469f01e
Aman Thukral <thukral618@gmail.com>
2020-06-30 05:19:23 +0530

    commit on 'master'

--- src/components/Option.js
+++ src/components/Option.js
@@ -5,9 +5,11 @@ import React from 'react';
 const Option = (props) => (
     <div>
         {props.optionText}
-        <button onClick={(e) => {
-            props.handleDeleteOption(props.optionText);
-        }}
+        <button
+            className="button button--link"
+            onClick={(e) => {
+                props.handleDeleteOption(props.optionText);
+            }}
         >
             remove
             </button>
