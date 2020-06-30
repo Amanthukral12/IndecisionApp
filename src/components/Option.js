694107fdf048e93d75a2d5b86a8b415e876cf4d8
Aman Thukral <thukral618@gmail.com>
2020-06-30 06:08:10 +0530

    commit on 'master'

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 
 const Option = (props) => (
-    <div>
+    <div className="option">
         {props.optionText}
         <button
             className="button button--link"
