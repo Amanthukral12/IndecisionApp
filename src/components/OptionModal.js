13a60046caf249d061966e3dfec06817c6963fbb
Aman Thukral <thukral618@gmail.com>
2020-07-02 01:32:29 +0530

    commit

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleDeleteOptionModel}
         contentLabel="Selected Option"
+        closeTimeoutMS={300}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleDeleteOptionModel}>Okay</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleDeleteOptionModel}>Okay</button>
     </Modal>
 );
 
