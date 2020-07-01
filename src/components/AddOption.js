1d5ac2b522ff8da17b2f1af8b214053f71c5923d
Aman Thukral <thukral618@gmail.com>
2020-07-01 23:59:15 +0530

    commit on 'master'

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -20,7 +20,7 @@ export default class AddOption extends React.Component {
     render() {
         return (
             <div>
-                {this.state.error && <p>{this.state.error}</p>}
+                {this.state.error && <p className="add-option-error">{this.state.error}</p>}
                 <form onSubmit={this.handleAddOption}>
                     <input type="text" name="option" />
                     <button className="button">Add Option</button>
--- src/components/Option.js
+++ src/components/Option.js
@@ -4,7 +4,8 @@ import React from 'react';
 
 const Option = (props) => (
     <div className="option">
-        {props.optionText}
+        <p className="option__text">{props.count}. {props.optionText}</p>
+
         <button
             className="button button--link"
             onClick={(e) => {
